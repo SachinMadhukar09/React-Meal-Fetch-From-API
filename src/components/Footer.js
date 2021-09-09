c5833b3d3f5eecac4803e0fdb9a012c33bbfc479
Sachin Madhukar <sachinkumarmadhukar@gmail.com>
2021-09-09 09:37:44 +0530

    Changed the title of App

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer(){
+    return (
+        <div style={{backgroundColor:'crimson'}} >
+            <p style={{color:'white'}} >Developed by Sachin Madhukar</p>
+            
+        </div>
+    )
+}
+export default Footer;
